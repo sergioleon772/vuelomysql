a68f5a7beaf4c565bad0f5ae31831a724fc0c90c
sergio <sergioleon10@outlook.es>
2025-03-13 16:28:29 -0300

    subieods

--- ejecutar.py
+++ ejecutar.py
@@ -411,7 +411,7 @@ def logout():
 if __name__ == "__main__":
     
 
-     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)))
+     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
      #app.run(debug=True, port=5001)
 
 
